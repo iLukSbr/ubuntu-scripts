c36bd7f786774a453c9b082f35289ec09590aa36
iLukSbr <lucasyfm@hotmail.com>
2025-11-03 02:14:53 +0000

    cublas

--- install_cuda.sh
+++ install_cuda.sh
@@ -28,6 +28,7 @@ else
     echo "CUDA LD_LIBRARY_PATH already in $HOME/.bashrc"
 fi
 
+# cuDNN
 wget https://developer.download.nvidia.com/compute/cudnn/9.14.0/local_installers/cudnn-local-repo-ubuntu2204-9.14.0_1.0-1_amd64.deb
 sudo dpkg -i cudnn-local-repo-ubuntu2204-9.14.0_1.0-1_amd64.deb
 sudo cp /var/cudnn-local-repo-ubuntu2204-9.14.0/cudnn-*-keyring.gpg /usr/share/keyrings/
@@ -35,3 +36,9 @@ sudo apt-get update
 sudo apt-get -y install cudnn
 
 nvcc --version
+
+# cuBLAS
+curl https://developer.download.nvidia.com/hpc-sdk/ubuntu/DEB-GPG-KEY-NVIDIA-HPC-SDK | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-hpcsdk-archive-keyring.gpg
+echo 'deb [signed-by=/usr/share/keyrings/nvidia-hpcsdk-archive-keyring.gpg] https://developer.download.nvidia.com/hpc-sdk/ubuntu/amd64 /' | sudo tee /etc/apt/sources.list.d/nvhpc.list
+sudo apt-get update -y
+sudo apt-get install -y nvhpc-25-9
